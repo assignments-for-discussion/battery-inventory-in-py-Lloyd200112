234fe8d93192f1d0421e49cc84532e38987f683f
Lloyd200112 <118347661+Lloyd200112@users.noreply.github.com>
2023-11-03 17:16:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,32 @@
+def classify_batteries(present_capacities):
+    healthy_count = 0
+    exchange_count = 0
+    failed_count = 0
 
-def count_batteries_by_health(present_capacities):
-  return {
-    "healthy": 0,
-    "exchange": 0,
-    "failed": 0
-  }
+    for present_capacity in present_capacities:
+        rated_capacity = 120  
+        soh = (present_capacity / rated_capacity) * 100
 
+        if soh > 80:
+            healthy_count += 1
+        elif soh >= 62:
+            exchange_count += 1
+        else:
+            failed_count += 1
 
-def test_bucketing_by_health():
-  print("Counting batteries by SoH...\n")
-  present_capacities = [113, 116, 80, 95, 92, 70]
-  counts = count_batteries_by_health(present_capacities)
-  assert(counts["healthy"] == 2)
-  assert(counts["exchange"] == 3)
-  assert(counts["failed"] == 1)
-  print("Done counting :)")
+    return healthy_count, exchange_count, failed_count
 
+def test_classify_batteries():
+    present_capacities_1 = [110, 115, 118, 120]
+    assert classify_batteries(present_capacities_1) == (4, 0, 0)
 
-if __name__ == '__main__':
-  test_bucketing_by_health()
+    present_capacities_2 = [105, 98, 75, 80, 90]
+    assert classify_batteries(present_capacities_2) == (3, 1, 1)
+
+    present_capacities_3 = [55, 60, 58, 50]
+    assert classify_batteries(present_capacities_3) == (0, 0, 4)
+
+    present_capacities_4 = []
+    assert classify_batteries(present_capacities_4) == (0, 0, 0)
+
+test_classify_batteries()
