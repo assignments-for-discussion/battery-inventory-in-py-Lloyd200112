db663a617cede1afdace22739085e243c2e91efd
Lloyd200112 <118347661+Lloyd200112@users.noreply.github.com>
2023-11-03 17:26:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 def classify_batteries(present_capacities):
+    if not present_capacities:
+        return 0, 0, 0
     healthy_count = 0
     exchange_count = 0
     failed_count = 0
 
     for present_capacity in present_capacities:
-        rated_capacity = 120  
+        if present_capacity < 0 or present_capacity > 120:
+            raise ValueError("Present capacity out of range [0, 120]")
+
+        rated_capacity = 120
         soh = (present_capacity / rated_capacity) * 100
+
+        if soh > 100:
+            soh = 100
 
         if soh > 80:
             healthy_count += 1
@@ -28,5 +36,16 @@ def test_classify_batteries():
 
     present_capacities_4 = []
     assert classify_batteries(present_capacities_4) == (0, 0, 0)
+
+    present_capacities_5 = [125, -10, 60, 121]
+    try:
+        classify_batteries(present_capacities_5)
+    except ValueError:
+        pass
+    else:
+        raise AssertionError("Expected ValueError for out-of-range present capacity")
+
+    present_capacities_6 = [130, 135]
+    assert classify_batteries(present_capacities_6) == (2, 0, 0)
 
 test_classify_batteries()
